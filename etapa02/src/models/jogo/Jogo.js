409c7f2dc147d26127ecd918519a3e4e9ba724ca
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-21 10:36:52 -0300

    style: organization of game comments and deletion of the repository file

--- etapa02/src/models/jogo/Jogo.js
+++ etapa02/src/models/jogo/Jogo.js
@@ -1,12 +1,17 @@
-//Define a classe Jogo 
-class Jogo {          // Gabi
-    constructor(grupos = null) {             // Gabi
-        this.alunos = []; // Lista de alunos cadastrados-Gabi
+// Definição da classe Jogo  
+class Jogo { // Criação da classe - Gabi
+
+    // Não há necessidade de declarar os atributos agora, - Samuel
+    // pois eles serão inicializados no construtor. - Samuel
+    // Dessa forma, o JavaScript entende que estes atributos já existem. - Samuel
+
+    constructor(grupos = null) { // Criação do constructor - Gabi
+        this.alunos = []; // Lista de alunos cadastrados - Gabi
         this.jogadores = []; // Lista de jogadores após o início do jogo - Alexandra
-        this.grupos = null; // Quantidade total de grupos (padrão: 6) - Alexandra
+        this.grupos = null; // Quantidade total de grupos (vazio) - Alexandra
         this.chat = new Chat(); // Gerencia o histórico de mensagens entre os jogadores - Jéssica 
     }
 }
 
-export default Jogo;
+export default Jogo; // Jéssica
 
