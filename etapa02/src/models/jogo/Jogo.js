d6e5527efff61c3084a9e7d7f5168ce4430f6015
Alexandraaversani <158167102+Alexandraaversani@users.noreply.github.com>
2024-11-21 10:29:05 -0300

    continuação da classe jogo

--- etapa02/src/models/jogo/Jogo.js
+++ etapa02/src/models/jogo/Jogo.js
@@ -1,7 +1,9 @@
-//Define a classe Jogo
-class Jogo {
-    constructor(grupos = null) {
+//Define a classe Jogo 
+class Jogo {          // Gabi
+    constructor(grupos = null) {             // Gabi
         this.alunos = []; // Lista de alunos cadastrados-Gabi
+        this.jogadores = []; // Lista de jogadores após o início do jogo - Alexandra
+        this.grupos = null; // Quantidade total de grupos (padrão: 6) - Alexandra
     }
 }
 
