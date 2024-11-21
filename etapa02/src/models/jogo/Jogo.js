e98984d86a8acc0691c228c9ca058e60fdbc6085
jessicaprestelo1201 <jessica.p.jesus@aluno.senai.br>
2024-11-21 10:32:59 -0300

    Feat: Finalização da classe Jogo

--- etapa02/src/models/jogo/Jogo.js
+++ etapa02/src/models/jogo/Jogo.js
@@ -4,6 +4,9 @@ class Jogo {          // Gabi
         this.alunos = []; // Lista de alunos cadastrados-Gabi
         this.jogadores = []; // Lista de jogadores após o início do jogo - Alexandra
         this.grupos = null; // Quantidade total de grupos (padrão: 6) - Alexandra
+        this.chat = new Chat(); // Gerencia o histórico de mensagens entre os jogadores - Jéssica 
     }
 }
+
+export default Jogo;
 
